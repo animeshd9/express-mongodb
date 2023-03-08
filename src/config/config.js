c85bfccbcb773219da88824a00eeaca4bb2aa0ed
Animesh Das <animeshd838@gmail.com>
2023-03-09 00:04:22 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ const path = require( 'path' );
 module.exports.getConfig = () => {
     const config = {
         'PORT': process.env.PORT || 5000,
-        'MONGO_URL': process.env.MONGO_URL || 'mongodb+srv://Animesh:mongodb@cluster0.tc9y3.mongodb.net/Broadifi?retryWrites=true&w=majority',
+        'MONGO_URL': process.env.MONGO_URL || 'mongo_url',
         'UPLOAD_PATH': path.resolve( `${__dirname }/../uploads` ),
         'JWT_SECRET': process.env.JWT_SECRET || 'R4ND0M5TR1NG'
     };
